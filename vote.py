70b632b9a8b4728e30736614a6e81f18acc1183e
manipatel636673-lang <manipatel636673@gmail.com>
2025-10-17 08:47:24 +0530

    Add age verification for voting eligibility

---
+++ vote.py
@@ -0,0 +1,10 @@
+#vote.py
+try:
+  age=int(input("enter your age : "))
+  if age >= 18:
+    print("your are eligible to vote")
+  else:
+    print("you are not eligible to vote")
+except ValueError:
+  print("please enter a valid integer")
+  
